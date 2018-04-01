624dd6490e18ff99566d1c634a3feded69fb0642
Matt Dawson <mdawsondev@gmail.com>
2018-04-01 15:51:05 -0400

    Chage cap for clarity

--- src/modules/ScrapeSite.js
+++ src/modules/ScrapeSite.js
@@ -2,10 +2,10 @@ import GitHub from './site-plugins/GitHub';
 import StackOverflow from './site-plugins/StackOverflow';
 
 async function ScrapeSite(req) {
-  const reqLC = req.toLowerCase();
+  const reqLc = req.toLowerCase();
   let site = () => `"${req}" is not a valid request.`;
-  if (reqLC === "github") site = GitHub;
-  if (reqLC === "stackoverflow") site = StackOverflow;
+  if (reqLc === "github") site = GitHub;
+  if (reqLc === "stackoverflow") site = StackOverflow;
   return await site();
 }
 
