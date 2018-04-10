05f8f9ffd7f26a44c176806a7f0c3534336ed35a
Matt Dawson <mdawsondev@gmail.com>
2018-04-10 19:25:49 -0400

    Include icons for visual

--- src/modules/cards/CardBody.js
+++ src/modules/cards/CardBody.js
@@ -4,13 +4,27 @@ export default class CardFuncs extends React.Component {
   render() {
     return (
       <div className="Card-Body">
-      <p className="Card-Body-Company"><span className="Card-Detail">Company:</span> <a href={this.props.cardProps.rawCard.glassdoor} target="_blank">{this.props.cardProps.rawCard.company}</a></p>
-      <p className="Card-Body-Posted"><span className="Card-Detail">Posted at:</span> {this.props.cardProps.rawCard.posted}</p>
-      <p className="Card-Body-Location"><span className="Card-Detail">Location:</span> <a href={this.props.cardProps.rawCard.citydataLoc} target="_blank">{this.props.cardProps.rawCard.location}</a></p>
+      
+      <p className="Card-Body-Company">
+        <span className="Card-Detail">Company: </span>
+        <a className="Card-Detail-Link" href={this.props.cardProps.rawCard.glassdoor} target="_blank">{this.props.cardProps.rawCard.company}</a>
+        &nbsp;<i className="fas fa-external-link-alt fa-xs" style={{opacity: .3}}aria-hidden="true"></i>        
+      </p>
+      
+      <p className="Card-Body-Posted">
+        <span className="Card-Detail">Posted at: </span> {this.props.cardProps.rawCard.posted}</p>
+      
+      <p className="Card-Body-Location">
+        <span className="Card-Detail">Location: </span>
+        <a className="Card-Detail-Link" href={this.props.cardProps.rawCard.citydataLoc} target="_blank">{this.props.cardProps.rawCard.location }</a>
+        &nbsp;<i className="fas fa-external-link-alt fa-xs" style={{opacity: .3}}aria-hidden="true"></i>
+      </p>
+
       <details className="Card-Body-Description">
         <summary>Read Description</summary>
         <span dangerouslySetInnerHTML={{__html: this.props.cardProps.rawCard.description}}></span>
       </details>
+      
       </div>
     )
   }
--- src/modules/cards/CardFuncs.js
+++ src/modules/cards/CardFuncs.js
@@ -7,9 +7,18 @@ export default class CardFuncs extends React.Component {
   render() {
     return (
       <div className="Card-Functions">
-        <p className="Card-Hide" onClick={this.hide}>{this.props.cardState.isHidden ? 'Unhide' : 'Hide'}</p>
-        <p className="Card-Star" onClick={this.save}>{this.props.cardState.isFavorite ? 'Unsave' : 'Save'}</p>
-        <p className="Card-Apply"><a href={this.props.cardProps.rawCard.app_url} target="_blank">Apply to Job</a></p>
+        <p className="Card-Hide" onClick={this.hide}>
+          <i className="fas fa-eye-slash" style={{opacity: .3}}aria-hidden="true"></i>&nbsp;
+          {this.props.cardState.isHidden ? 'Unhide' : 'Hide'}
+        </p>
+        <p className="Card-Star" onClick={this.save}>
+          <i className="fas fa-star" style={{opacity: .3}}aria-hidden="true"></i>&nbsp;
+          {this.props.cardState.isFavorite ? 'Unsave' : 'Save'}
+          </p>
+        <p className="Card-Apply">
+          <i className="fas fa-thumbs-up" style={{opacity: .3}}aria-hidden="true"></i>&nbsp;
+          <a href={this.props.cardProps.rawCard.app_url} target="_blank">Apply to Job</a>
+        </p>
       </div>
     )
   }
